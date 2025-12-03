31b58eec406a2ab841f85d063748aed2053cd215
Mohammad Naeem <mohammad.naeem@telusinternational.com>
2025-12-03 11:57:26 +0530

    Connect db

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import app from "./app.js";
+import connectDB from "./src/config/database.js";
 
 const port = process.env.PORT;
+
 app.get("/", (req, res) => {
   res.json({ value: "Hello World!" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on url http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on url http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {});
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1);
   }
 };
